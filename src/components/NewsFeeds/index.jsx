bb3007e7a3dea57d57587655b4fd2ff1cfb5362d
Humida <huynhminhdanh1110@gmail.com>
2021-06-06 22:23:41 +0700

    fix bug

--- src/components/NewsFeeds/index.jsx
+++ src/components/NewsFeeds/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import News from "./news";
+import Recommends from "./recommends";
+import "../../style/newfeeds.css";
 
 NewFeeds.propTypes = {};
 
 function NewFeeds(props) {
-  return <div>day lka trang home</div>;
+  return (
+    <div className="newfeeds">
+      <News />
+      <Recommends />
+    </div>
+  );
 }
 
 export default NewFeeds;
---
+++ src/components/NewsFeeds/news.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// import sub component
+import Post from "./news/post";
+
+News.propTypes = {};
+
+function News(props) {
+  return (
+    <div className="news">
+      <Post />
+    </div>
+  );
+}
+
+export default News;
