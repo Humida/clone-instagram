7e13906e270ff4a99062b71c1e012ceff3015930
Humida <huynhminhdanh1110@gmail.com>
2021-06-07 09:46:19 +0700

    create post component

--- src/components/NewsFeeds/news/post.jsx
+++ src/components/NewsFeeds/news/post.jsx
@@ -8,12 +8,14 @@ function Post(props) {
     <div className="post">
       <div className="post__header">
         <div className="post__header--author">
-          <img src="https://via.placeholder.com/64x64" alt="" />
-          <h2 className="post__header--author--name"></h2>
+          <img src="https://via.placeholder.com/36x36" alt="" />
+          <h2 className="post__header--author--name">__Doopoo</h2>
         </div>
         <div className="post__header--more">...</div>
       </div>
-      <div className="post__media"></div>
+      <div className="post__media">
+        <img src="https://via.placeholder.com/600x600" alt="" />
+      </div>
       <div className="post__content">
         <div className="post__content--dialog"></div>
         <div className="post__content--quantity-dialog"></div>
